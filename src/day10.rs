54c4e6c6c383ec0afa81816947524ae32d9adeff
egelmex <ellism88@gmail.com>
2022-12-14 17:59:19 +0000

    misisng files

---
+++ src/day10.rs
@@ -0,0 +1,77 @@
+use crate::read_lines::read_lines_unwrapped;
+
+const COL_WIDTH: isize = 40;
+
+pub fn part1() {
+    const CHECK: [usize; 6] = [20, 60, 100, 140, 180, 220];
+    let input = read_lines_unwrapped("inputs/mine/day10.txt");
+
+    let mut x: isize = 1;
+    let mut c: usize = 0;
+    let mut res = 0;
+    for i in input {
+        if i == "noop" {
+            c += 1;
+            if CHECK.contains(&c) {
+                res += x * c as isize;
+            }
+        } else if i.starts_with("addx ") {
+            let add: isize = i.strip_prefix("addx ").unwrap().parse::<isize>().unwrap();
+            c += 1;
+            if CHECK.contains(&c) {
+                res += x * c as isize;
+            }
+            c += 1;
+            if CHECK.contains(&c) {
+                res += x * c as isize;
+            }
+            x += add;
+        }
+
+        //dbg!(x, c);
+    }
+    println!("{}", res);
+}
+
+pub fn part2() {
+    let input = read_lines_unwrapped("inputs/examples/day10_2.txt");
+
+    let mut x: isize = 1;
+    let mut c: usize = 0;
+
+    let mut screen = [' '; 40 * 6];
+    for i in input {
+        if i == "noop" {
+            c += 1;
+            screen[c - 1] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
+                '#'
+            } else {
+                ' '
+            };
+        } else if i.starts_with("addx ") {
+            let add: isize = i.strip_prefix("addx ").unwrap().parse::<isize>().unwrap();
+            c += 1;
+
+            screen[c - 1] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
+                '#'
+            } else {
+                ' '
+            };
+            c += 1;
+
+            x += add;
+            screen[c - 1] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
+                '#'
+            } else {
+                ' '
+            };
+        }
+
+        //dbg!(x, c);
+    }
+
+    let rows = screen.chunks(COL_WIDTH as usize);
+    for row in rows {
+        println!("{}", String::from_iter(row));
+    }
+}
