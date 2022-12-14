2b657dbac970bd41101bc5b241a23d9c68cf8350
egelmex <ellism88@gmail.com>
2022-12-14 19:23:49 +0000

    day10

--- src/day10.rs
+++ src/day10.rs
@@ -34,16 +34,22 @@ pub fn part1() {
 }
 
 pub fn part2() {
-    let input = read_lines_unwrapped("inputs/examples/day10_2.txt");
+    let input = read_lines_unwrapped("inputs/mine/day10.txt");
 
     let mut x: isize = 1;
     let mut c: usize = 0;
 
-    let mut screen = [' '; 40 * 6];
+    let mut screen = [' '; (40 * 6) + 1];
+    screen[c] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
+        '#'
+    } else {
+        ' '
+    };
+
     for i in input {
         if i == "noop" {
             c += 1;
-            screen[c - 1] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
+            screen[c] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
                 '#'
             } else {
                 ' '
@@ -52,7 +58,7 @@ pub fn part2() {
             let add: isize = i.strip_prefix("addx ").unwrap().parse::<isize>().unwrap();
             c += 1;
 
-            screen[c - 1] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
+            screen[c] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
                 '#'
             } else {
                 ' '
@@ -60,7 +66,7 @@ pub fn part2() {
             c += 1;
 
             x += add;
-            screen[c - 1] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
+            screen[c] = if [x - 1, x, x + 1].contains(&(&(c as isize) % COL_WIDTH)) {
                 '#'
             } else {
                 ' '
@@ -72,6 +78,6 @@ pub fn part2() {
 
     let rows = screen.chunks(COL_WIDTH as usize);
     for row in rows {
-        println!("{}", String::from_iter(row));
+        println!(" - {}", String::from_iter(row));
     }
 }
